6484cf1caa2f6c0b20d9422df938dea848c67b0f
Mia2804 <yl4287@columbia.edu>
2021-04-22 19:30:39 -0400

    bootstrap-sandbox babel

---
+++ JS/JsSet.js
@@ -0,0 +1,18 @@
+// 1. Compare two objects
+var obj1 = {
+    id: 1,
+    name: "John"
+}
+var obj2 = {
+    id:1,
+    name:"Will"
+}
+console.log(obj1.id == obj2.id, obj1.name == obj2.name)
+
+// copy to clipboard
+
+
+// csv to 2D array
+
+
+// csv to 2D array of objs, the first row of the string is used as the title row
