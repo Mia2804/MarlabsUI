2a2b5020c955cb16c4b96764ff2dec1f78a114ce
Mia2804 <yl4287@columbia.edu>
2021-04-25 22:13:21 -0400

    server

--- NodeJS/read_write.js
+++ NodeJS/read_write.js
@@ -5,16 +5,23 @@ var fs = require('fs')
 // console.log(read)
 // fs.writeFileSync('write.txt', read)
 
-// fs.readFile('data.txt', 'utf-8', function(err,data) {
-//     if(err) { console.log(err.message) }
-//     else { console.log(data) }
-// })
+fs.readFile('data.txt', 'utf-8', function(err,data) {
+    if(err) { console.log(err.message) }
+    else { console.log(data) }
+})
 
 // removing a file
 // fs.unlink('w.txt', (err) => {
 //     console.log(err.message)})
 
 // removing a directory
-fs.rmdir('a', (err) => {
-    if(err) console.log(err.message)
-})
+// fs.rmdir('a', (err) => {
+//     if(err) console.log(err.message)
+// })
+// fs.rmdir('a', { recursive: true }, (err) => {
+//     if (err) {
+//         throw err;
+//     }
+
+//     console.log('a is deleted!');
+// });
--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,10 +1,12 @@
 var http = require('http')
+var fs = require('fs')
+// var data = fs.readFileSync('data.txt', 'utf-8')
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hellow world');
-    
-});
-
-server.listen(3000, '127.0.0.1');
+fs.readFile('data.txt', function(err, data) {
+    if (err) throw err // Fail if the file can't be read.
+    http.createServer(function(req, res) {
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.end(data) // Send the file data to the browser.
+    }).listen(3000, '127.0.0.1')
+})
 console.log("Server running at port 3000")
